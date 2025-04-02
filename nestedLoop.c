e83e6fddbe783e90ff507eb0e038b83abfafe2b7
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-04-02 17:07:49 +0530

    Create nestedLoop.c

---
+++ nestedLoop.c
@@ -0,0 +1,97 @@
+
+#include <stdio.h>
+
+int main()
+{
+    // nested loop
+    // for (int i=1;i<=5;i++)
+    // {
+    //     for(int j=1;j<=5;j++)
+    //     {
+    //         printf("i=%d , j=%d\n",i,j);
+    //     }
+    // }
+    
+    
+    // for (int i=1;i<5;i++)
+    // {
+    //     for(int j=1;j<=i;j++)
+    //     {
+    //         printf("i=%d , j=%d");
+    //     }
+    // }
+    
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+    
+    // for (int row=1;row<=5;row++)
+    // {
+    //     for (int col=1;col<=5;col++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    
+    // for (int row=1;row<=5;row++)
+    // {
+    //     for (int col=1;col<=row;col++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+    
+    // swapping - interchange
+    // int x = 22;
+    // int y = 10;
+    
+    // int temp ;
+    // printf("before swapping : x=%d , y=%d",x,y);
+    // temp = x;
+    // x = y;
+    // y = temp;
+    // printf("after swapping : x=%d , y=%d",x,y);
+    
+    // 1
+    // 22
+    // 333
+    // 4444
+    // 55555
+    
+    // 1
+    // 12
+    // 123
+    // 1234
+    // 12345
+    
+    // 1
+    // 01
+    // 010
+    // 1010
+    // 10101
+    
+    //     *
+    //   **
+    //   ***
+    //  ****
+    // *****
+    
+    // *   *
+    // *   *
+    // *****
+    // *   *
+    // *   *
+    
+    return 0;
+}
