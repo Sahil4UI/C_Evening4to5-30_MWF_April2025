5837fdef05d2b6b3d3beb18f4138f8df1a8c2b83
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-04-21 17:09:06 +0530

    Update nestedLoop.c

--- nestedLoop.c
+++ nestedLoop.c
@@ -1,67 +1,49 @@
-
+// Online C compiler to run C program online
 #include <stdio.h>
 
-int main()
-{
-    // nested loop
+int main() {
+// nested loop
     // for (int i=1;i<=5;i++)
     // {
-    //     for(int j=1;j<=5;j++)
+    //     for (int j=1;j<=5;j++)
     //     {
-    //         printf("i=%d , j=%d\n",i,j);
+    //         printf("i = %d , j =  %d \n",i,j);
     //     }
+        
     // }
     
     
-    // for (int i=1;i<5;i++)
+    // for (int i=1;i<=5;i++)
     // {
-    //     for(int j=1;j<=i;j++)
+    //     for (int j=1;j<=i;j++)
     //     {
-    //         printf("i=%d , j=%d");
+    //         printf("i = %d , j =  %d \n",i,j);
     //     }
+        
     // }
     
-    // *****
-    // *****
-    // *****
-    // *****
-    // *****
-    
-    // for (int row=1;row<=5;row++)
+    //     for (int i=1;i<=5;i++)
     // {
-    //     for (int col=1;col<=5;col++)
+    //     for (int j=1;j<=i;j++)
     //     {
     //         printf("*");
     //     }
     //     printf("\n");
+        
     // }
-    
-    
-    // *
-    // **
-    // ***
-    // ****
-    // *****
-    
-    // for (int row=1;row<=5;row++)
+    // 1
+    // 12
+    // 123
+    // 1234
+    // 12345
+    // for (int i=1;i<=5;i++)
     // {
-    //     for (int col=1;col<=row;col++)
+    //     for (int j=1;j<=i;j++)
     //     {
-    //         printf("*");
+    //         printf("%d",j);
     //     }
     //     printf("\n");
     // }
-    
-    // swapping - interchange
-    // int x = 22;
-    // int y = 10;
-    
-    // int temp ;
-    // printf("before swapping : x=%d , y=%d",x,y);
-    // temp = x;
-    // x = y;
-    // y = temp;
-    // printf("after swapping : x=%d , y=%d",x,y);
     
     // 1
     // 22
@@ -69,11 +51,14 @@ int main()
     // 4444
     // 55555
     
-    // 1
-    // 12
-    // 123
-    // 1234
-    // 12345
+    //  for (int i=1;i<=5;i++)
+    // {
+    //     for (int j=1;j<=i;j++)
+    //     {
+    //         printf("%d",i);
+    //     }
+    //     printf("\n");
+    // }
     
     // 1
     // 01
@@ -81,17 +66,39 @@ int main()
     // 1010
     // 10101
     
+// int x= 1;
+//  for (int i=1;i<=5;i++)
+//     {
+//         for (int j=1;j<=i;j++)
+//         {
+//             if (x%2==0){
+//                 printf("0");
+//             }
+//             else{
+//                 printf("1");
+//             }
+//             x++;
+//         }
+//         printf("\n");
+//     }
     //     *
-    //   **
+    //    **
     //   ***
     //  ****
     // *****
     
-    // *   *
-    // *   *
-    // *****
-    // *   *
-    // *   *
+    // for (int i=1;i<=5;i++)
+    // {
+    //     for (int k=1;k<=5-i;k++)
+    //     {
+    //         printf(" ");
+    //     }
+    //     for (int j=1;j<=i;j++)
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
     
     return 0;
 }
